e01f3607fbe781a3d84af784d03fff7708a030f8
Kyle Denver Morena <kyledenvermorena@gmail.com>
2021-03-09 20:09:46 +0100

    install all dependencies and get api as test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, {useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const api = process.env.REACT_APP_API_KEY;
+  useEffect(()=>{
+    getBooksApi();
+  },[])
+
+  const getBooksApi = async () => {
+    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=${api}`)
+    console.log(response.data);
+  }
   return (
     <div className="App">
       <h1>hello</h1>
+      <button className='btn' onClick={getBooksApi}>prova</button>
     </div>
   );
 }
