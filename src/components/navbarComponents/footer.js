890193943af55edcf0ca93958504f49a4841ac8b
Kyle Denver Morena <kyledenvermorena@gmail.com>
2021-04-30 18:56:19 +0200

    add Lorenzo Laluna on footer

--- src/components/navbarComponents/footer.js
+++ src/components/navbarComponents/footer.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Copyright = () => {
   return (
     <footer className="blockquote mt-5 text-light">
-      &copy;2021 Designed and Developed by &nbsp;
+      &copy;{new Date().getFullYear()} Designed by &nbsp;
+      <strong title="SourceTitle">
+        <a href="https://www.linkedin.com/in/lorenzo-laluna" target="blank">Lorenzo Laluna</a>
+      </strong>
+      &nbsp; and Developed by &nbsp;
       <strong title="SourceTitle"> 
         <a href="https://kyle-morena.web.app/" target="blank">Kyle Denver Morena</a>
       </strong>
